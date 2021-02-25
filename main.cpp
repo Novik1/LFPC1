513711a6060b0ed3b2347285b188ecb727807ea6
Novik1 <71001348+Novik1@users.noreply.github.com>
2021-02-25 10:14:50 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,17 +7,17 @@ using namespace std;
 int main()
 {
     HashMap ht;
-    int s = 0, a = 0, b = 0 , c = 0;
+    int s = 0, b = 0 , d = 0;
     string word;
-    read(ht,s,d,r);
+    read(ht,s,b,d);
     cout<<"Grammar Rules:"<<endl;
     ht.get('S',s);
-    ht.get('D',a);
-    ht.get('R',b);
-    ht.get('R',c);
+    ht.get('B',b);
+    ht.get('D',d);
+    
     cout<<"Input a word to check:   ";
     cin>>word;
-    if (ht.check(word,s,a,b,c)) cout << word << " is accepted";
+    if (ht.check(word,s,b,d)) cout << word << " is accepted";
     else cout << word << " is not accepted";
 
 }
